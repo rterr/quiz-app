3b778297765e80a2e9c90c605e069df412b5ba0f
R. Terrado <r.terrado@gmail.com>
2016-07-21 19:57:15 -0700

    UI complete, pseudo-code roadmap complete

--- app.js
+++ app.js
@@ -1,13 +1,51 @@
 $(document).ready(function() {
-		// variables
-		var questionCounter = 0;
-		var scoreCounter = 0;
+	// variables
+	var questionCounter = -1;
+	var scoreCounter = 0;
+	shuffle(questionList);
 
-	  $("#next-question").click(function() {
-			questionCounter++;
-			$('div#start-page').toggle();
-			$('span#question-counter-number').text(questionCounter);
-	   	$('#question-page h2').text(questionList[questionCounter].question);
-	  });
+	//start next question
+	 $('span.next-question').click(function() {
+		questionCounter++;
 
+		$(this).parent('div').toggle();
+		//add if statement that checks if questionCounter>5
+		//if questionCounter>4, then toggle on the result-final div and output the correctCounter to it
+		//else continue with displaying the next question
+		$('span#question-counter-number').text(questionCounter + 1);
+		$('span#score-counter-number').text(scoreCounter);
+   		$('#question-page h2').text(questionList[questionCounter].question);
+	 });
+
+	//user selects an answer
+	$('span.answer-option').click(function(){
+		$('div#result-page').toggle();
+		//if then statement
+		//if id = questionList[questionCounter].correct (it's right)
+
+		//else (it's wrong)
+	 });
+
+	//user selects new game
+	$('span.new-game').click(function(){
+		//reset the questionCounter and scoreCounter, run shuffle(questionList);
+		// toggle off (this).parent('div') and toggle on start-page div
+	});
+
+//end	
 });
+
+//Fisher-Yates shuffle
+function shuffle(array) {
+	var m = array.length, t, i;
+	// While there remain elements to shuffle…
+	while (m) {
+		// Pick a remaining element…
+		i = Math.floor(Math.random() * m--);
+		// And swap it with the current element.
+		t = array[m];
+		array[m] = array[i];
+		array[i] = t;
+		}
+  	return array;
+	}
